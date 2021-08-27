b405c9853bb1bb6c4aed822017b8130e1ea4694d
ichaaaa <golinski.kamil88@gmail.com>
2021-08-27 15:58:48 +0200

    e8a2924

---
+++ precache-manifest.f5b0788fd6b6c92f0d8ed41d2bd618b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7261fe1f41be79b54978",
+    "url": "/css/app.ba014d55.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f73ac99f66f7a961c579e9f99cc7d586",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20724a1d24dcb1886e52",
+    "url": "/js/about.f3673ced.js"
+  },
+  {
+    "revision": "7261fe1f41be79b54978",
+    "url": "/js/app.7325e336.js"
+  },
+  {
+    "revision": "1ce9a6db599ba4c6e6ed",
+    "url": "/js/chunk-vendors.313d81b9.js"
+  },
+  {
+    "revision": "7e5cfb04b53db6a7ee7222d8bc5d0253",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
